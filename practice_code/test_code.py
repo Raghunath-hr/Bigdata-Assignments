d7c82474f6b6b8fd5779715f809a47fe173672dc
bhagwant mane <raghunathpanse@gmail.com>
2022-12-01 01:56:08 +0530

    new modification

--- practice_code/test_code.py
+++ practice_code/test_code.py
@@ -1,2 +1,12 @@
 import pandas as pd
 #this file is handled by git cloning from bash terminal
+"""
+1-first always check git status by running below command
+    git status
+2-If you see file is modified you have add changes by
+    git add .
+3-after this you need to commit the code using command
+    git commit -m "pass any message"
+4-and last step is to push the code to repository
+    git push origin main
+"""
